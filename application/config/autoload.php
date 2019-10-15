ce42efa1811959e5ba186cbd8a5efc5ca34e432f
SgtBagus <procw57@gmail.com>
2019-10-15 16:08:08 +0700

    asd

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -132,4 +132,4 @@ $autoload['language'] = array();
 |
 |	$autoload['model'] = array('first_model' => 'first');
 */
-$autoload['model'] = array('mymodel','mlogin','mmodel', 'mloginadmin');
+$autoload['model'] = array('mymodel','mlogin','mmodel');
--- application/views/admin/index.php
+++
@@ -1,8 +0,0 @@
-<div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-    <h1>Dashboard</h1>
-      <hr/>
-      <!-- <h5 style="padding-left:1px;">Welcome to SmartSoft Dashboard</h5> -->
-    </section>
-</div>
